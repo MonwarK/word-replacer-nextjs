cedfa6e2908910062a419e4317209fc025e05308
Monwar <monwar259@gmail.com>
2021-11-25 19:24:48 +0000

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import 'tailwindcss/tailwind.css'
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="min-h-screen bg-gray-300">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,75 @@
-import Head from 'next/head'
+import { useState } from "react"
+import ReactMarkdown from 'react-markdown'
+import { Editor, EditorState } from 'draft-js';
 
 export default function Home() {
+  const [replaceWord, setReplaceWord] = useState("")
+  const [newWord, setNewWord] = useState("")
+  const [textToSimplify, setTextToSimplify] = useState("")
+  const [convertedText, setConvertedText] = useState("")
+
+  const handleClick = () => {
+    const paragraphArray = textToSimplify.split(" ")
+    let paragraphArray2 = [];
+    
+    paragraphArray.map((item) => {
+        if(item.toLowerCase().includes(replaceWord.toLowerCase())) {
+            paragraphArray2.push(newWord)
+        } else {
+            paragraphArray2.push(item)
+        }
+    })
+
+    console.log(paragraphArray2)
+    setConvertedText(paragraphArray2.join(" "))
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="max-w-screen-md mx-auto bg-gray-100 w-full min-h-screen p-14">
+      {/* Header */}
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      {/* Ad */}
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      {/* Replace Text */}
+      <div>
+        <div className="grid grid-cols-2 space-x-5">
+          <div>
+            <p>Replace Word</p>
+            <input 
+              className="py-1 px-2 outline-none bg-yellow-200 shadow-inner w-full" 
+              type="text"
+              onChange={(e) => setReplaceWord(e.target.value)}
+            />
+          </div>
+          <div>
+            <p>New Word</p>
+            <input 
+              className="py-1 px-2 outline-none bg-yellow-200 shadow-inner w-full" 
+              type="text"
+              onChange={(e) => setNewWord(e.target.value)}
+            />
+          </div>
         </div>
-      </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+        <div className="my-5">
+          <p>Enter text to simplify:</p>
+          <textarea 
+            rows="8" 
+            className="bg-yellow-200 shadow-inner w-full outline-none p-2"
+            onChange={(e) => setTextToSimplify(e.target.value)}  
+          >
+          </textarea>
+        </div>
+
+        <button onClick={handleClick} className="bg-gray-800 text-white py-2 px-7 rounded-lg">Convert</button>
+      </div>
+
+      {convertedText && (
+        <div className="my-10">
+          <h1 className="text-3xl font-bold">Your output</h1>
+          <ReactMarkdown>{convertedText}</ReactMarkdown>
+        </div>
+      )}
     </div>
   )
 }
